6ba8fb7b80872849301b66f87bd04f53c1c5a25c
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-11 23:06:44 -0700

    added script to app js

--- restaurants/src/App.js
+++ restaurants/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './script.js'
 
 function App() {
   return (
--- restaurants/src/script.js
+++ restaurants/src/script.js
@@ -1,3 +1,3 @@
 const getRestaurant = function(){
-    
+    let apiURL = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
 }
