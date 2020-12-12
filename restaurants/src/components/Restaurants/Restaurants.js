4b5825235981b3ce1b0aa030a99be8a613c672ba
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-12 16:13:20 -0700

    added newItem funtion to capture and render data. first function renderItems added to Restaurants component

---
+++ restaurants/src/components/Restaurants/Restaurants.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NewItem from './NewItem.js';
+
+export default class Restaurants extends Component {
+    render() {
+        return (
+            <ul>
+                {this.renderItems()}
+            </ul>
+            <div>
+                
+            </div>
+        )
+    }
+}
--- restaurants/src/script.js
+++
@@ -1,5 +0,0 @@
-const getRestaurant = function(){
-    let apiURL = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
-}
-
-//how to pass key by using the HTTP Basic authentication header or by sending an api_key parameter via the query string or request body. 
