4d968be6116107cfdfd1eeac8eab7ce9cd1bb9d6
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-12 14:41:39 -0700

    Checked api via postman, 200. Added .env file to protect key later, added api helper component

--- restaurants/src/App.js
+++ restaurants/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import './script.js'
+require('dotenv').config();
+
+console.log(process.env)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+                <p>
+          Edit <code>src/App.js</code> and save to reload. 
         </p>
         <a
           className="App-link"
