d1efd6dd83df18fb1242c2bf4e4ba21c08574de6
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-12 15:55:27 -0700

    added app components edited index

---
+++ restaurants/src/api.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class api extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+// ux sees  name city state ph and genre
+//alphebetical order
+// filter; state genre - defauly to all instantly (no additional clicks
+//user shuold be able to clear search results by clearing text value in search input
+//user should see 10 results, and should pageinate
+// user should be able to combine filter and search  - should be able to turn filters on and off while search value is present
+// response of NO RESULTS FOUND  when empty
