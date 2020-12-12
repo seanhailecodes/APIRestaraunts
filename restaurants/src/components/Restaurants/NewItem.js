344a115ad237d241c094411cadeb61fccb6cae5c
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-12 16:37:19 -0700

    added stateless component NewItem to help manage api

---
+++ restaurants/src/components/Restaurants/NewItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const  NewItem = ({item}) => (
+    <li>
+        <p>
+            {item.name}
+        </p>
+    </li>
+)
+
+export default NewItem;
--- restaurants/src/components/Restaurants/Restaurants.js
+++ restaurants/src/components/Restaurants/Restaurants.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import NewItem from './NewItem.js';
 
 export default class Restaurants extends Component {
+
+    renderItems(){
+        return this.props.items.map((item) => (
+            <NewItem key={item.id} item={item} />
+        ))
+    }
+
     render() {
         return (
             <ul>
