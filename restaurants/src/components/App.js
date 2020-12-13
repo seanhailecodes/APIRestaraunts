8e8778dc6d044a22af3abb79c0b2055ddf1cd42d
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-12 18:14:29 -0700

    coded fetch function which failed CORS

--- restaurants/src/components/App.js
+++ restaurants/src/components/App.js
@@ -8,18 +8,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-                <p>
-          Edit <code>src/App.js</code> and save to reload. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 className='App-title'> Charter's Code Test - Restaurants</h3>
       </header>
+      <Restaurants />
     </div>
   );
 }
--- restaurants/src/components/Restaurants/Restaurants.js
+++ restaurants/src/components/Restaurants/Restaurants.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 import NewItem from './NewItem.js';
 
 export default class Restaurants extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            restaurants: [],
+        }
+    }
+
+    componentDidMount() {
+        const url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants&Api-Key q3MNxtfep8Gt'
+
+        fetch(url)
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                this.setState({
+                    restaurants: data.name 
+                })
+            })
+            .catch((error) => console.log(error))
+    }
 
     renderItems(){
-        return this.props.items.map((item) => (
+        return this.state.restaurants.map((item) => (
             <NewItem key={item.id} item={item} />
         ))
     }
