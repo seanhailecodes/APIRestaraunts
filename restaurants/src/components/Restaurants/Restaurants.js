9b8a2f6c8320e449383847813734561683cb46ab
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-13 00:36:36 -0700

    api data now renders

--- restaurants/src/components/Restaurants/Restaurants.js
+++ restaurants/src/components/Restaurants/Restaurants.js
@@ -10,36 +10,32 @@ export default class Restaurants extends Component {
     }
 
     componentDidMount() {
-        const url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
-        const API_KEY = 'Api-Key q3MNxtfep8Gt'
 
-        fetch(url, {
-            headers: {
-                Authorization: API_KEY,
-            }
-        } )
-            .then((response) => {
-                return response.json()
-            })
-            .then((data) => {
-                this.setState({
-                    restaurants: data.name 
-                })
-            })
-            .catch((error) => console.log(error))
-    }
+    let url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
+    let API_KEY = 'Api-Key q3MNxtfep8Gt'
 
-    renderItems(){
-        return this.state.restaurants.map((item) => (
-            <NewItem key={item.id} item={item} />
-        ))
-    }
+    fetch(url, {
+        headers: {
+            Authorization: API_KEY,
+        }
+    } )
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {console.log(data)
+           
+        })
+
+}
 
     render() {
+
+        
+
         return (
-            <ul>
-                {this.renderItems()}
-            </ul>
+            <div>
+                
+            </div>
         )
     }
 }
