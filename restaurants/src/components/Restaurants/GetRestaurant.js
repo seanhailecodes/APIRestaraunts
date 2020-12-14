b43b6533b2b71641e141e260d1878d10edd340c3
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-13 23:37:59 -0700

    added button to getrestaurant

--- restaurants/src/components/Restaurants/GetRestaurant.js
+++ restaurants/src/components/Restaurants/GetRestaurant.js
@@ -8,9 +8,20 @@ export default class GetRestaurant extends Component {
         return (
             <Search>
                 <div>
-                    <input onChange={e => this.props.GetRestaurant(e.target.value)}/>
+                    <input id='getRestaurant' type='text' className='getRestaurant' onChange={e => this.props.GetRestaurant(e.target.value)}/>
                 </div>
-                </Search>
+            </Search>
+
+
+            <Filter>
+                <button>
+{/* //This button may need more elements, test it */}
+<a class="waves-effect waves-light btn">button</a>
+<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+<a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>
+                </button>
+        
+            </Filter>
         )
     }
 }
