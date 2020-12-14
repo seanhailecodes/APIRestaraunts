56fe423203dd1501210bfecf997bf9591bd24caa
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-13 22:31:21 -0700

    commented renderItems they are not rendering atm

--- restaurants/src/components/Restaurants/Restaurants.js
+++ restaurants/src/components/Restaurants/Restaurants.js
@@ -1,49 +1,54 @@
-import React, { Component } from 'react';
-import NewItem from './NewItem.js';
 
-export default class Restaurants extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            restaurants: [],
-            states: [],
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        restaurants: [],
+        states: [],        
+    };
+
+  }
+
+  componentDidMount() {
+
+    let url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
+    let API_KEY = 'Api-Key q3MNxtfep8Gt'
+
+    fetch(url, {
+        headers: {
+            Authorization: API_KEY,
         }
-    }
-
-    componentDidMount() {
-
-        let url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
-        let API_KEY = 'Api-Key q3MNxtfep8Gt'
-
-        fetch(url, {
-            headers: {
-                Authorization: API_KEY,
-            }
+    })
+        .then((response) => {
+            return response.json()
         })
-            .then((response) => {
-                return response.json()
+        .then((data) => {
+            this.setState({
+                restaurants: data
             })
-            .then((data) => {
-                this.setState({
-                    restaurants: data
-                })
-            })
-            .catch((error) => console.log(error))
-    }
-
-
-    renderItems() {
-        return this.state.restaurants.map((data) => (
-            <NewItem key={data.id} item={data} key={data.state.genre} item={data} />
-        ))
-    }
-
-    render() {
-
-        return (
-            <div className='row'>
-                {this.renderItems()}
-            </div>
-        )
-    }
+        })
+        .catch((error) => console.log(error))
 }
+
+  render() {
+
+ 
+
+  
+
+
+
+    return (
+
+        <div className='row'>
+        {/* {this.renderItems()} */}
+    </div>
+
+    );
+  }
+}
+
+export default App;
