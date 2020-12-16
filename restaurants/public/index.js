6bbd6148640e8f489b02f79d7cc77f4feb79aa2d
seanhailecodes <seanhaile.codes@gmail.com>
2020-12-16 11:04:17 -0700

    relocated indexhtml

--- restaurants/public/index.js
+++ restaurants/public/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import App from '../src/Components/App';
 import 'bootstrap/dist/css/bootstrap.css'
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from '../src/reportWebVitals';
 
 require('dotenv').config();
 
